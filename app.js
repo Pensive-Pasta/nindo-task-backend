6c6ae09e33e86255ec13d0a55d7aa0164a22a0ce
PensivePasta <pensivepasta@gmail.com>
2023-12-04 20:48:14 +0000

    Set up and connected MongoDB Atlas.

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
 const express = require("express");
-const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 
+const app = express();
 app.use(express.json());
+
+const uri = process.env.MONGODB_URI;
+
+const client = new MongoClient(uri, {
+  serverApi: ServerApiVersion.v1
+});
+
+async function run() {
+  try {
+    await client.connect();
+    console.log("Connected successfully to MongoDB");
+  } catch (err) {
+    console.error('Could not connect to MongoDB', err);
+  }
+}
+
+run().catch(console.dir);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
