c4a48a6d06f01bc3731194b01fe2c248f0e050f1
PensivePasta <pensivepasta@gmail.com>
2023-12-05 09:59:23 +0000

    Add CRUD operations with MongoDB integration.

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
 const express = require("express");
-const { MongoClient, ServerApiVersion } = require('mongodb');
-require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
 
 const app = express();
 app.use(express.json());
 
 const uri = process.env.MONGODB_URI;
-
 const client = new MongoClient(uri, {
-  serverApi: ServerApiVersion.v1
+  serverApi: ServerApiVersion.v1,
 });
 
-async function run() {
-  try {
-    await client.connect();
-    console.log("Connected successfully to MongoDB");
-  } catch (err) {
-    console.error('Could not connect to MongoDB', err);
+client.connect((err, client) => {
+  if (err) {
+    console.error("Failed to connect to MongoDB", err);
+    return;
   }
-}
+  console.log("Connected successfully to MongoDB");
 
-run().catch(console.dir);
+  const db = client.db("nindo-task-db");
+  const taskRoutes = require("./routes/tasks")(db);
+  app.use("/api/tasks", taskRoutes);
 
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  const PORT = process.env.PORT || 3001;
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+});
---
+++ routes/tasks.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const router = express.Router();
+const { ObjectId } = require("mongodb");
+
+module.exports = function (db) {
+  const collection = db.collection("tasks");
+
+  router.get("/", async (req, res) => {
+    try {
+      const tasks = await collection.find({}).toArray();
+      res.json(tasks);
+    } catch (e) {
+      res.status(500).send("Failed to fetch tasks.");
+    }
+  });
+
+  router.post("/", async (req, res) => {
+    try {
+      const result = await collection.insertOne(req.body);
+      res.status(201).json(result);
+    } catch (e) {
+      res.status(500).send("Failed to create a new task.");
+    }
+  });
+
+  router.put("/:id", async (req, res) => {
+    try {
+      const result = await collection.updateOne(
+        { _id: new ObjectId(req.params.id) },
+        { $set: req.body }
+      );
+      if (result.matchedCount === 0) {
+        return res.status(404).send("Task not found.");
+      }
+      res.json(result);
+    } catch (e) {
+      res.status(500).send("Failed to update the task.");
+    }
+  });
+
+  router.delete("/:id", async (req, res) => {
+    try {
+      const result = await collection.deleteOne({
+        _id: new ObjectId(req.params.id),
+      });
+      if (result.deletedCount === 0) {
+        return res.status(404).send("Task not found.");
+      }
+      res.json(result);
+    } catch (e) {
+      res.status(500).send("Failed to delete the task.");
+    }
+  });
+
+  return router;
+};
